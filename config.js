7d78ef842cacb47e6e4d74a17a8386b1359eaecb
oded <odedmoran222@gmail.com>
2015-11-30 18:22:57 +0200

    change db source

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
 
   'secret': 'ilovescotchyscotch',
-  // 'database': 'mongodb://localhost/token'
-  'database': 'mongodb://odedmo:Madbull22@ds041394.mongolab.com:41394/todos'
+  'database': 'mongodb://localhost/token'
 };
