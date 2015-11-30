c770345752f7617cf750de289ed0a58bce89acee
oded <odedmoran222@gmail.com>
2015-11-30 18:26:02 +0200

    rechange

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
 
   'secret': 'ilovescotchyscotch',
-  'database': 'mongodb://localhost/token'
+  // 'database': 'mongodb://localhost/token'
+  'database': 'mongodb://odedmo:Madbull22@ds041394.mongolab.com:41394/todos'
 };
